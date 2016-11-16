40257ceaf0aeefb86c20da009bf9d88cc20e3997
racheltstevens <racheltstevens@gmail.com>
2016-11-16 11:53:58 -0500

    Add routes for patients
    For cohort 015

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,12 @@ Rails.application.routes.draw do
   delete '/sign-out/:id' => 'users#signout'
   patch '/change-password/:id' => 'users#changepw'
   resources :users, only: [:index, :show]
+
+  # Patient routes
+  get '/patients', to: 'patients#index'
+  post '/patients', to: 'patients#create'
+
+  get '/patients/:id', to: 'patients#show'
+  patch '/patients/:id', to: 'patients#update'
+  delete '/patients/:id', to: 'patients#destroy'
 end
